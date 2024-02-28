43759ad3253be60dd338cee058fb87b139f8fa72
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2024-02-28 21:55:23 +0000

    chore: introduce ui lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,11 +10,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={"min-h-screen bg-background font-sans antialiased"}>
+        {children}
+      </body>
     </html>
   );
 }
