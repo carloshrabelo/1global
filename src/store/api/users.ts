91d2872e23898aa51d700a929c98c56de6438377
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2024-03-03 01:16:08 +0000

    feat: introduce user update API

--- src/store/api/users.ts
+++ src/store/api/users.ts
@@ -10,7 +10,55 @@ export const userApi = baseApi.injectEndpoints({
         params,
       }),
     }),
+    createUsers: build.mutation<
+      User,
+      Omit<User, "id" | "avatar"> & PaginationParams
+    >({
+      // page only for cache
+      query: ({ page: _page, ...body }) => ({
+        url: "users",
+        method: "POST",
+        body,
+      }),
+      async onQueryStarted({ page }, { dispatch, queryFulfilled }) {
+        const response = await queryFulfilled;
+        const patchResult = dispatch(
+          userApi.util.updateQueryData(
+            "getUsers",
+            { page },
+            (draft: PaginationResponse<User>) => ({
+              ...draft,
+              data: [...draft.data, response.data],
+            }),
+          ),
+        );
+        queryFulfilled.catch(patchResult.undo);
+      },
+    }),
+    updateUser: build.mutation<User, User & PaginationParams>({
+      // page only for cache
+      query: ({ id, page: _page, ...body }) => ({
+        url: `users/${id}`,
+        method: "PUT",
+        body,
+      }),
+      onQueryStarted({ id, page, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          userApi.util.updateQueryData(
+            "getUsers",
+            { page },
+            (draft: PaginationResponse<User>) => ({
+              ...draft,
+              data: draft.data.map((item) =>
+                item.id === id ? { ...item, ...patch } : item,
+              ),
+            }),
+          ),
+        );
+        queryFulfilled.catch(patchResult.undo);
+      },
+    }),
   }),
 });
 
-export const { useGetUsersQuery } = userApi;
+export const { useGetUsersQuery, useUpdateUserMutation } = userApi;
