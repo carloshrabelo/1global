4f3e88c0f751648ffcfce813ed06fc4595081be8
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2024-03-05 17:58:48 +0000

    chore: gh-pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone'
+
+const exportConfig = {
+  basePath: '/1global',
+  output: 'export'
+}
+
+const nextConfig = process.env.BUILD_EXPORT ? exportConfig : {
+  output: 'standalone',
 };
 
 export default nextConfig;
