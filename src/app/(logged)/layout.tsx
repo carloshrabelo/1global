c2d4025415dc11add20b8b7f31d98f31042cc348
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2024-03-05 18:08:16 +0000

    fix: autenticate user in static page

--- src/app/(logged)/layout.tsx
+++ src/app/(logged)/layout.tsx
@@ -1,10 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+
 import Navbar from "@/components/Navbar";
-import { ReactNode } from "react";
+import { verifyAuthFE } from "@/utils/auth";
 
 export default function LoggedLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  return (
+  const router = useRouter();
+  const isAuth = verifyAuthFE();
+
+  // Validade auth in FE for Static Pages
+  useEffect(() => {
+    !isAuth && router.push("signin");
+  }, [isAuth, router]);
+
+  return !isAuth ? null : (
     <div className="h-full flex flex-col">
       <Navbar />
       <div className="flex-1">{children}</div>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,15 @@
-import { USER_TOKEN } from "@/utils/constants";
 import { NextRequest } from "next/server";
 import Cookies from "universal-cookie";
+
+import { USER_TOKEN } from "@/utils/constants";
 
 const cookies = new Cookies();
 
 export const login = (token: string) => cookies.set(USER_TOKEN, token);
 
 export const logout = () => cookies.remove(USER_TOKEN);
+
+export const verifyAuthFE = () => !!cookies.get(USER_TOKEN);
 
 export const verifyAuth = (req: NextRequest) => {
   const token = req.cookies.get(USER_TOKEN);
